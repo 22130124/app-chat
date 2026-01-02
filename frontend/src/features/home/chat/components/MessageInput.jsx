703d0f8d9a3ec0f466cfa3f327b09203a8475467
hathingan <22130177@st.hcmuaf.edu.vn>
2026-01-02 11:00:51 +0700

    xử lý xong gửi emoji

--- frontend/src/features/home/chat/components/MessageInput.jsx
+++ frontend/src/features/home/chat/components/MessageInput.jsx
@@ -1,12 +1,30 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import styles from "./MessageInput.module.scss";
 import { Send } from "lucide-react";
-import { MdAddAPhoto, MdVideoLibrary } from "react-icons/md";
+import { MdAddAPhoto, MdVideoLibrary, MdEmojiEmotions } from "react-icons/md";
 import { useSendMessage } from "../hooks/useSendMessage.js";
 import { useSendImage } from "../hooks/useSendImage.js";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useSendVideo } from "../hooks/useSendVideo.js";
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
+import { useChatPicker } from "../hooks/useChatPicker.js";
+import { useSendEmoji } from "../hooks/useSendEmoji.js";
+
+
+const LOCAL_STICKERS = [
+  "/stickers/sticker1.png",
+  "/stickers/sticker2.png",
+  "/stickers/sticker3.png",
+  "/stickers/sticker4.png",
+  "/stickers/sticker5.png",
+  "/stickers/sticker6.png",
+  "/stickers/sticker7.png",
+  "/stickers/sticker8.png",
+];
+
+
 
 export const MessageInput = ({ placeholder = "Nhập tin nhắn..." }) => {
   const [message, setMessage] = useState("");
@@ -15,7 +33,15 @@ export const MessageInput = ({ placeholder = "Nhập tin nhắn..." }) => {
   const { sendMessage } = useSendMessage();
   const { sendImage } = useSendImage();
   const videoInputRef = useRef(null);
+
   const { sendVideo } = useSendVideo();
+  const { showPicker, togglePicker, tab, switchTab } = useChatPicker();
+
+  const popupRef = useRef(null);
+  const buttonRef = useRef(null);
+  const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
+  const { handleSelectEmoji } = useSendEmoji();
+
 
   const handleSend = () => {
     if (!message.trim()) return;
@@ -36,6 +62,9 @@ export const MessageInput = ({ placeholder = "Nhập tin nhắn..." }) => {
     videoInputRef.current.click();
   };
 
+  const handleSelectSticker = (sticker) =>
+      setMessage(prev => prev + `[sticker:${sticker}]`);
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
@@ -43,55 +72,143 @@ export const MessageInput = ({ placeholder = "Nhập tin nhắn..." }) => {
     }
   };
 
-  return (
-    <footer className={styles.inputArea}>
-      <div className={styles.inputWrapper}>
-        <button
-          className={styles.uploadImageButton}
-          onClick={handleUploadAndSendImage}
-        >
-          <MdAddAPhoto size={20} />
-        </button>
-        <button
-          className={styles.uploadImageButton}
-          onClick={handleUploadAndSendVideo}
-        >
-          <MdVideoLibrary size={20} />
-        </button>
-        <input
-          type="text"
-          placeholder={
-            currentChatUser ? placeholder : "Chọn một cuộc trò chuyện..."
-          }
-          className={styles.input}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          disabled={!currentChatUser}
-        />
-        <button
-          className={styles.sendButton}
-          onClick={handleSend}
-          disabled={!message.trim() || !currentChatUser}
-        >
-          <Send size={20} />
-        </button>
+  // Tính vị trí popup khi mở
+  useEffect(() => {
+    if (showPicker && buttonRef.current) {
+      const button = buttonRef.current;
+      const rect = buttonRef.current.getBoundingClientRect();
+      setPopupPosition({
+        top: button.offsetTop - 380, // 250px popup + 10px margin
+        left: button.offsetLeft - 30
+      });
+    }
+  }, [showPicker]);
 
-        <input
-          type="file"
-          accept="image/*"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={sendImage}
-        />
-        <input
-          type="file"
-          accept="video/*"
-          ref={videoInputRef}
-          style={{ display: "none" }}
-          onChange={sendVideo}
-        />
-      </div>
-    </footer>
+  // Click ngoài popup thì ẩn popup đi
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (showPicker && popupRef.current &&
+          !popupRef.current.contains(event.target) &&
+          buttonRef.current && !buttonRef.current.contains(event.target)
+
+    ){
+        togglePicker(false); // ẩn popup
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [showPicker,togglePicker]);
+
+
+  return (
+      <footer className={styles.inputArea}>
+        <div className={styles.inputWrapper}>
+          <button
+              className={styles.uploadImageButton}
+              onClick={handleUploadAndSendImage}
+          >
+            <MdAddAPhoto size={20} />
+          </button>
+          <button
+              className={styles.uploadImageButton}
+              onClick={handleUploadAndSendVideo}
+          >
+            <MdVideoLibrary size={20} />
+          </button>
+
+          {/* Nút Emoji/Sticker */}
+          <button className={styles.chatPickerButton}
+                  onClick={togglePicker}
+                  ref={buttonRef} >
+            <MdEmojiEmotions size={20} />
+          </button>
+
+          {/* Popup Emoji/Sticker */}
+          {showPicker && (
+              <div className={styles.chatPickerPopup}
+                ref={popupRef}
+                style={{
+                  position: "absolute",
+                  top: popupPosition.top,
+                  left: popupPosition.left,
+              }}
+                    >
+
+                <div className={styles.tabButtons}>
+                  <button
+                      className={tab === "emoji" ? styles.activeTab : ""}
+
+                      onClick={() => switchTab("emoji")}
+                  >
+                    Emoji
+                  </button>
+                  <button
+                      className={tab === "sticker" ? styles.activeTab : ""}
+                      onClick={() => switchTab("sticker")}
+                  >
+                    Sticker
+                  </button>
+                </div>
+
+                <div className={styles.tabContent}>
+                  {tab === "emoji" && (
+                      <Picker
+                          data={data}
+                          onEmojiSelect={(emoji) => handleSelectEmoji(emoji, setMessage)}
+                          theme="light"
+                          style={{ width: "100%", height: "250px" }}
+                      />
+                  )}
+                  {tab === "sticker" && (
+                      <div className={styles.stickerGrid}>
+                        {LOCAL_STICKERS.map((sticker, i) => (
+                            <img
+                                key={i}
+                                src={sticker}
+                                alt="sticker"
+                                onClick={() => handleSelectSticker(sticker)}
+                            />
+                        ))}
+                      </div>
+                  )}
+                </div>
+              </div>
+          )}
+
+          <input
+              type="text"
+              placeholder={
+                currentChatUser ? placeholder : "Chọn một cuộc trò chuyện..."
+              }
+              className={styles.input}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              disabled={!currentChatUser}
+          />
+          <button
+              className={styles.sendButton}
+              onClick={handleSend}
+              disabled={!message.trim() || !currentChatUser}
+          >
+            <Send size={20} />
+          </button>
+
+          <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={sendImage}
+          />
+          <input
+              type="file"
+              accept="video/*"
+              ref={videoInputRef}
+              style={{ display: "none" }}
+              onChange={sendVideo}
+          />
+        </div>
+      </footer>
   );
 };
